153dbfc4005b5cfd89ee9c9b7ba02895385edb33
Дмитрий Егоров <desure85@gmail.com>
2015-09-30 17:01:16 +0300

    double_fix

--- frontend/themes/default2/views/site/index.php
+++ frontend/themes/default2/views/site/index.php
@@ -153,8 +153,6 @@ use yii\jui\Slider;
                     $outer .= '<div  class="container-fluid float" id="index-card-1"><div data-prod="'.$product[products_id].'" id="prod-data-img"  style="clear: both; min-height: 180px; min-width: 200px; background-size:cover; background: no-repeat scroll 50% 50% / contain url(/site/imagepreview?src='.$product[products_image].');"></div><div class="name">'.$description[products_name].'</div><div class="model">Арт.'.$product[products_model].'</div><div class="price"><b>'.intval($product[products_price]).'</b> руб.</div><div id="prod-info" data-prod="'.$product[products_id].'">Инфо</div><span>'.$attr_html.'</span></div>';
                     echo $outer;
                 }
-
-
                 ?>
             </div>
         </div>
