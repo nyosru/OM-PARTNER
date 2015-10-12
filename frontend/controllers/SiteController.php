16043a1df5e0df87a0ce4c174315427f35b21165
Дмитрий Егоров <desure85@gmail.com>
2015-10-12 11:25:47 +0300

    Preview Image Refresh action

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -866,9 +866,6 @@ class SiteController extends Controller
         $src = Yii::$app->request->getQueryParam('src');
         $action = Yii::$app->request->getQueryParam('action', 'none');
         $src = urldecode($src);
-        $filename = str_replace('[[[[]]]]',' ', $src);
-        $filename = str_replace('[[[[','(', $src);
-        $filename = str_replace(']]]]',')', $src);
         $split = explode('/', $src);
         if(count($split) > 1 ) {
             $file = array_splice($split, -1,1);
