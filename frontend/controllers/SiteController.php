2803c2c34dab958c5a9fa1154fba5fb8e0927e45
Дмитрий Егоров <desure85@gmail.com>
2015-10-12 10:44:04 +0300

    Reformat menu view

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -254,7 +254,7 @@ class SiteController extends Controller
         $hide_man = implode(',' , $list);
 
         if($prod_attr_query == '' && $searchword == ''){
-           $x =  PartnersProductsToCategories::find()->select('MAX(products.`products_last_modified`) as products_last_modified')->distinct()->JoinWith('products')->where('categories_id IN ('.$cat.')')->limit($count)->offset($start_arr)->asArray()->one();
+           $x =  PartnersProductsToCategories::find()->select('MAX(products.`products_last_modified`) as products_last_modifieds ')->distinct()->JoinWith('products')->where('categories_id IN ('.$cat.')')->asArray()->one();
        if(!isset($x['products_last_modified'])){
            $checkcache = '0000-00-00';
        }else {
@@ -262,10 +262,10 @@ class SiteController extends Controller
 
        }
             $data = Yii::$app->cache->get(urlencode('first-'.$cat_start.'-'.$hide_man.'-'.$start_price.'-'.$end_price.'-'.$count.'-'.$start_arr.'-'.$sort));
-            if(isset($data) && $checkcache !== $data['checkcache']){
+            if(isset($data) && $checkcache != $data['checkcache']){
                  Yii::$app->cache->delete(urlencode('first-'.$cat_start.'-'.$hide_man.'-'.$start_price.'-'.$end_price.'-'.$count.'-'.$start_arr.'-'.$sort));
             }
-           if ($data === false || $checkcache !== $data['checkcache']) {
+           if ($data === false || $checkcache != $data['checkcache']) {
 
                $productattrib = PartnersProductsToCategories::find()->select(['products_options_values.products_options_values_id','products_options_values.products_options_values_name'])->distinct()->JoinWith('products')->where('categories_id IN ('.$cat.')  and products.removable != 1   and products.products_quantity > 0   and products_status=1  and products_price <= :end_price and products_price >= :start_price  and products.manufacturers_id NOT IN ('.$hide_man.')  ',[':start_price' => $start_price, ':end_price' => $end_price])->orderBy('`products_price` DESC')->JoinWith('productsDescription')->JoinWith('productsAttributes')->JoinWith('productsAttributesDescr')->asArray()->all();
                $count_arrs = PartnersProductsToCategories::find()->JoinWith('products')->where('categories_id IN ('.$cat.') and products_status=1  and products.products_quantity > 0   and products_price <= :end_price and products.removable != 1   and products_price >= :start_price  and products.manufacturers_id NOT IN ('.$hide_man.')',[':start_price' => $start_price, ':end_price' => $end_price])->groupBy(['products.`products_id` DESC'])->count();
