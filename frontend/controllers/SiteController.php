7a10d27788e39c7b8320ea78828fae1ae6238fcc
Дмитрий Егоров <desure85@gmail.com>
2015-10-13 13:11:53 +0300

    Change count products in page

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -291,7 +291,7 @@ class SiteController extends Controller
             $x =  PartnersProductsToCategories::find()->select('MAX(products.`products_last_modified`) as products_last_modifieds ')->distinct()->JoinWith('productsDescription')->JoinWith('products')->where('categories_id IN ('.$cat.')')->asArray()->one();
             $prod =  PartnersProductsToCategories::find()->select('products.products_id as prod,  products.products_last_modified as last ')->JoinWith('products')->where(' ( categories_id IN ('.$cat.')) and (products_status = 1) and (products_image IS NOT NULL) and ( products.products_quantity > 1 )  and (products_price <= :end_price) and (products_price >= :start_price)  and (products.manufacturers_id NOT IN ('.$hide_man.'))',[':start_price' => $start_price, ':end_price' => $end_price])->limit($count)->offset($start_arr)->JoinWith('productsDescription')->JoinWith('productsAttributes')->JoinWith('productsAttributesDescr')->groupBy(['products.`products_id` DESC'])->distinct()->orderBy($orders)->asArray()->all();
             foreach($prod as $values){
-                $dataprod = Yii::$app->cache->get(urlencode('prod-'.$values['prod']));
+                $dataprod = Yii::$app->cache->get(urlencode('prod'.$values['prod']));
                 if(isset($dataprod) && (date($dataprod['last']) - date($values['last'])) > 3600){
                     $data[] =  $dataprod['data'];
                 }else{
@@ -303,8 +303,8 @@ class SiteController extends Controller
                 $datar = PartnersProductsToCategories::find()->JoinWith('products')->where('products.products_id IN ('.$prodarr.')')->JoinWith('productsDescription')->JoinWith('productsAttributes')->JoinWith('productsAttributesDescr')->groupBy(['products.`products_id` DESC'])->distinct()->asArray()->all();
 
                 foreach($datar as $valuesr){
-                   Yii::$app->cache->delete(urlencode('prod-'.$valuesr['products']['products_id']), ['data' => $valuesr, 'last' => $valuesr['products']['products_last_modified']]);
-                    Yii::$app->cache->set(urlencode('prod-'.$valuesr['products']['products_id']), ['data' => $valuesr, 'last' => $valuesr['products']['products_last_modified']], 86400);
+                   Yii::$app->cache->delete(urlencode('prod'.$valuesr['products']['products_id']), ['data' => $valuesr, 'last' => $valuesr['products']['products_last_modified']]);
+                    Yii::$app->cache->set(urlencode('prod'.$valuesr['products']['products_id']), ['data' => $valuesr, 'last' => $valuesr['products']['products_last_modified']], 86400);
                 $data[] = $valuesr;
                 }
             }
