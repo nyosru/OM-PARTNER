392d6e58db6053b9d524aed7dfd1ff7c233a4f19
Дмитрий Егоров <desure85@gmail.com>
2015-10-13 16:32:55 +0300

    Change count products in page

--- frontend/themes/default2/views/layouts/main.php
+++ frontend/themes/default2/views/layouts/main.php
@@ -88,7 +88,7 @@ rmrevin\yii\fontawesome\AssetBundle::register($this);
         <hr style="width: 55%; position: absolute; left: 1px; top: 1px; border: 1px solid #00FFCC;">
         <div class="container">
         <p class="pull-left">&copy; Все права защищены, 2014-<?= date('Y') ?></p>
-<div style="margin:auto;">
+<div style="margin: 0% 25%; float: left;">
             <?php if(Yii::$app->user->can('admin')){CKEditorInline::begin(['preset' => 'standart']);}
             $data = new PartnersConfig();
             $check = Yii::$app->params['constantapp']['APP_ID'];
