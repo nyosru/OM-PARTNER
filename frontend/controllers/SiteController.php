2398aebe9b25aed2d029a5d339a498d8c50a466c
desure <desure85@gmail.com>
2015-10-14 22:58:01 +0300

    Change Cache system

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -281,7 +281,7 @@ class SiteController extends Controller
                 foreach ($prod as $values) {
                     $keyprod = Yii::$app->cache->buildKey('product-'.$values['prod']);
                     $dataprod = Yii::$app->cache->get($keyprod);
-                    if (isset($dataprod) && ( date($values['last']) - date($dataprod['last'])) == 0) {
+                    if (isset($dataprod) && ( date($values['last']) - date($dataprod['last'])) < 300) {
                         $data[] =  $dataprod['data'];
                     }else {
                         $nodata[] = $values['prod'];
