5a59ea4cc3edfca4d61cfb2f5509e95fc3116c68
Дмитрий Егоров <desure85@gmail.com>
2015-10-14 15:21:35 +0300

    Change cache system

--- frontend/themes/default2/views/site/indexpage.php
+++ frontend/themes/default2/views/site/indexpage.php
@@ -19,6 +19,7 @@ use common\models\PartnersCatDescription;
 use common\models\Manufacturers;
 use common\models\PartnersProductsToCategories;
 use frontend\controllers\ExtFunc;
+use yii\caching\Cache;
 $functions = new ExtFunc();
 
 //if ($this->beginCache('partner-index'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'] , array('duration'=>600))) {?>
@@ -67,23 +68,20 @@ $functions = new ExtFunc();
                 }
                  $hide_man = implode(',', $list);
                 $products = '960192894,95833167,95848445';
-                $dataproducts = new PartnersProductsToCategories();
+                $key = Yii::$app->cache->buildKey('index_optional');
+                $dataproducts = Yii::$app->cache->get($key);
+                if(!$dataproducts) {
+                    $dataproducts = new PartnersProductsToCategories();
+                    $dataproducts = $dataproducts->find()->JoinWith('products')->where('products_status=1  and products.products_quantity > 0    and products.manufacturers_id NOT IN (' . $hide_man . ')  and products.products_model IN (' . $products . ')')->JoinWith('productsDescription')->JoinWith('productsAttributes')->limit(3)->distinct()->JoinWith('productsAttributesDescr')->asArray()->all();
+                    Yii::$app->cache->set($key, $dataproducts, 86400);
+                }
 
-
-                $dataproducts = $dataproducts->find()->JoinWith('products')->where('products_status=1  and products.products_quantity > 0    and products.manufacturers_id NOT IN ('.$hide_man.')  and products.products_model IN ('.$products.')')->JoinWith('productsDescription')->JoinWith('productsAttributes')->limit(3)->distinct()->JoinWith('productsAttributesDescr')->asArray()->all();
-
-
-                if(isset($dataproducts[0])){
-                }else{  $dataproducts = "Не найдено";}
-
-
-
-
-                $newproducts = PartnersProductsToCategories::find()->JoinWith('products')->where('products_status=1  and products.products_quantity > 0    and products.manufacturers_id NOT IN ('.$hide_man.') ')->JoinWith('productsDescription')->JoinWith('productsAttributes')->distinct()->limit(3)->JoinWith('productsAttributesDescr')->orderBy('`products_date_added` DESC')->asArray()->all();
-
-
-                if(isset($newproducts[0])){
-                }else{  $newproducts = 'Не найдено!';}
+                $key = Yii::$app->cache->buildKey('index_new');
+                $newproducts = Yii::$app->cache->get($key);
+                if(!$newproducts) {
+                    $newproducts = PartnersProductsToCategories::find()->JoinWith('products')->where('products_status=1  and products.products_quantity > 0    and products.manufacturers_id NOT IN (' . $hide_man . ') ')->JoinWith('productsDescription')->JoinWith('productsAttributes')->distinct()->limit(3)->JoinWith('productsAttributesDescr')->orderBy('`products_date_added` DESC')->asArray()->all();
+                    Yii::$app->cache->set($key, $newproducts, 86400);
+                }
                 foreach($dataproducts as $value){
                     $outer = '';
                     $product = $value['products'];
