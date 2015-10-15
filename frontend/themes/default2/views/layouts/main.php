f0262c70a4bf526588218c6675708803fd21bd4f
Дмитрий Егоров <desure85@gmail.com>
2015-10-15 13:24:25 +0300

    Change cache system

--- frontend/themes/default2/views/layouts/main.php
+++ frontend/themes/default2/views/layouts/main.php
@@ -126,6 +126,21 @@ rmrevin\yii\fontawesome\AssetBundle::register($this);
     $this -> registerJsFile('/themes/'.Yii::$app->params['constantapp']['APP_THEMES'].'/js/script.js', ['depends'=> ['yii\web\JqueryAsset', 'yii\jui\JuiAsset']]);
     $this->endBody();
    ;?>
+    <!-- Rating@Mail.ru counter -->
+    <script type="text/javascript">
+        var _tmr = _tmr || [];
+        _tmr.push({id: "2698708", type: "pageView", start: (new Date()).getTime()});
+        (function (d, w, id) {
+            if (d.getElementById(id)) return;
+            var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
+            ts.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//top-fwz1.mail.ru/js/code.js";
+            var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
+            if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
+        })(document, window, "topmailru-code");
+    </script><noscript><div style="position:absolute;left:-10000px;">
+            <img src="//top-fwz1.mail.ru/counter?id=2698708;js=na" style="border:0;" height="1" width="1" alt="Рейтинг@Mail.ru" />
+        </div></noscript>
+    <!-- //Rating@Mail.ru counter -->
 </body>
 </html>
 <?php $this->endPage() ?>
