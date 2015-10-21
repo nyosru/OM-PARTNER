4d3e4917e9c6ccda770e5098e9797caed9018ee8
Дмитрий Егоров <desure85@gmail.com>
2015-10-21 13:19:01 +0300

    SEO

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -368,27 +368,28 @@ class SiteController extends Controller
     public function actionSearchword()
     {
         $filt = mb_strtolower(Yii::$app->request->getQueryParam('filt', NULL), mb_detect_encoding(Yii::$app->request->getQueryParam('filt', NULL)));
-        if ($filt != NULL) {
-            $query_filt = ' and  LOWER(products_name) RLIKE :searchword ';
-            $query_ar = [':searchword' => $filt];
-        } else {
-            $query_filt = ' ';
-            $query_ar = [];
-        }
         $key = Yii::$app->cache->buildKey('searchfullnamnes');
-        $test = Yii::$app->cache->get($key);
-        if (isset($test['data'])) {
-            $test = $test['data'];
-        } else {
-            $test = PartnersProducts::find()->select('products_name as name')->where('products_status = 1 and (products_image IS NOT NULL) and ( products.products_quantity > 0 ) ')->JoinWith('productsDescription')->distinct()->orderBy(['products_date_added' => SORT_DESC, 'products.products_id' => SORT_ASC])->asArray()->all();
-            Yii::$app->cache->set($key, ['data' => $test], 604800);
+        $key2 = Yii::$app->cache->buildKey('searchfullnamnes-'.$filt);
+        $testmain = Yii::$app->cache->get($key2);
+        if(!$testmain[data]) {
+            $test = Yii::$app->cache->get($key);
+            if (isset($test['data'])) {
+                $test = $test['data'];
+            } else {
+                $test = PartnersProducts::find()->select('products_name as name')->where('products_status = 1 and (products_image IS NOT NULL) and ( products.products_quantity > 0 ) ')->JoinWith('productsDescription')->distinct()->orderBy(['products_date_added' => SORT_DESC, 'products.products_id' => SORT_ASC])->asArray()->all();
+                Yii::$app->cache->set($key, ['data' => $test], 604800);
+            }
+            foreach ($test as $value) {
+                preg_match('/^[^\ \_\(\)\,\-\.\'\\\;\:\+\/"?]*(' . $filt . ')[^\ \_\(\)\,\-\.\'\\\;\:\+\/"?]*/iu', $value['name'], $output_array);
+                $preg[] = mb_strtolower($output_array[0], mb_detect_encoding($output_array[0]));
+            }
+            $preg = array_unique($preg);
+            $testout = implode('/////', $preg);
+            Yii::$app->cache->set($key2, ['data' => $testout], 604800);
+        }else{
+            $testout =   $testmain['data'];
         }
-        foreach ($test as $value) {
-            preg_match('/^[^\ \_\(\)\,\-\.\'\\\;\:\+\/"?]*(' . $filt . ')[^\ \_\(\)\,\-\.\'\\\;\:\+\/"?]*/iu', $value['name'], $output_array);
-            $preg[] = mb_strtolower($output_array[0], mb_detect_encoding($output_array[0]));
-        }
-        $preg = array_unique($preg);
-        $testout = implode('/////', $preg);
+
         return $testout;
 
     }
