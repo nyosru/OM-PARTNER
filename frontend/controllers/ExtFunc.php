fdda975b5267952efc17295e06fbe43c0d634fd8
Дмитрий Егоров <desure85@gmail.com>
2015-10-22 14:41:21 +0300

    SEO

--- frontend/controllers/ExtFunc.php
+++ frontend/controllers/ExtFunc.php
@@ -10,7 +10,7 @@ use common\models\PartnersCatDescription;
 
 class ExtFunc
 {
-     public function load_cat($arr, $parent_id = 0, $catnamearr, $allow_cat ) {
+    public function load_cat($arr, $parent_id = 0, $catnamearr, $allow_cat ) {
         static $str_load_cat;
         if (empty($arr[$parent_id])) {
 
@@ -19,35 +19,35 @@ class ExtFunc
                 $catdesc = $arr[$parent_id][$i]['categories_id'];
                 if (!$arr[$parent_id][$i] == '') {
                     $str_load_cat[] =  $catdesc;
-                   $this->load_cat($arr, $arr[$parent_id][$i]['categories_id'], $catnamearr, $allow_cat);
+                    $this->load_cat($arr, $arr[$parent_id][$i]['categories_id'], $catnamearr, $allow_cat);
                 }
             }
         }
-         if($parent_id != 0){
-             $str_load_cat[] =  $parent_id;
-         }
+        if($parent_id != 0){
+            $str_load_cat[] =  $parent_id;
+        }
 
         return  $str_load_cat;
     }
 
     public function view_cat($arr, $parent_id = 0, $catnamearr, $allow_cat) {
-         if (empty($arr[$parent_id])) {
-             return;
-         } else {
-             if ($parent_id !== 0) {$style = 'style="display: none;"';
-             }
-             echo '<ul id="accordion" class="accordion" ' . $style . '">';
-             for ($i = 0; $i < count($arr[$parent_id]); $i++) {
-                 $catdesc = $arr[$parent_id][$i]['categories_id'];
-                 if (!$arr[$parent_id][$i] == '') {
-                     echo '<li class=""><a href="/site/catalog?_escaped_fragment_=cat=' . $catdesc . '&count=20&start_price=&end_price=1000000&prod_attr_query=&page=undefined&sort=0&searchword="></a><div class="link data-j" data-j="on" data-cat="' . $catdesc . '">' . $catnamearr["$catdesc"] .'</div>';
+        if (empty($arr[$parent_id])) {
+            return;
+        } else {
+            if ($parent_id !== 0) {$style = 'style="display: none;"';
+            }
+            echo '<ul id="accordion" class="accordion" ' . $style . '">';
+            for ($i = 0; $i < count($arr[$parent_id]); $i++) {
+                $catdesc = $arr[$parent_id][$i]['categories_id'];
+                if (!$arr[$parent_id][$i] == '') {
+                    echo '<li class=""><a href="/site/catalog?_escaped_fragment_=cat=' . $catdesc . '&count=20&start_price=&end_price=1000000&prod_attr_query=&page=undefined&sort=0&searchword="></a><div class="link data-j" data-j="on" data-cat="' . $catdesc . '">' . $catnamearr["$catdesc"] .'</div>';
                     $this->view_cat($arr, $arr[$parent_id][$i]['categories_id'], $catnamearr, $allow_cat);
-                     echo '</li>';
-                 }
-             }
-             echo '</ul>';
-         }
-     }
+                    echo '</li>';
+                }
+            }
+            echo '</ul>';
+        }
+    }
 
     public function reformat_cat_array($catdata, $categories, $checks ){
         foreach ($catdata as $value) {
@@ -85,19 +85,19 @@ class ExtFunc
     }
     public function categories_for_partners()
     {
-
-
-        $dependency = new DbDependency([
-            'sql' => 'SELECT MAX(last_modified) FROM {{%categories}}',
-        ]);
-        $catdataarr = Yii::$app->db->cache(
-            function ($db) {
-                $categoriess = new PartnersCategories();
-                $categoriesd = new PartnersCatDescription();
-                return [$categoriess->find()->select(['categories_id', 'parent_id'])->where('categories_status != 0')->asArray()->All(), $categoriesd->find()->select(['categories_id', 'categories_name'])->asArray()->All()];
-            }, 3600, $dependency
-        );
-        return $catdataarr;
+        $categoriess = new PartnersCategories();
+        $categoriesd = new PartnersCatDescription();
+        $key = Yii::$app->cache->buildKey('categories');
+        $data = Yii::$app->cache->get($key);
+        if(!isset($data['data'])) {
+            $f = $categoriess->find()->select(['categories_id', 'parent_id'])->where('categories_status != 0')->asArray()->All();
+            $s = $categoriesd->find()->select(['categories_id', 'categories_name'])->asArray()->All();
+            $data = array($f,$s);
+            Yii::$app->cache->set($key,['data'=>$data],3600);
+        }else{
+            $data = $data['data'];
+        }
+        return $data;
     }
 
 }
