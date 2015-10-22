6624d1d54e68dd2fb4b75820afbcd69c25013964
Дмитрий Егоров <desure85@gmail.com>
2015-10-22 13:01:26 +0300

    SEO

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -292,7 +292,7 @@ class SiteController extends Controller
                 $d2=new \DateTime();
                 $d2->setTimestamp(strtotime(trim($dataprod['last'])));
                 $diff = $d2->diff($d1);
-                $marker = $diff->y+$diff->m+$diff->d+$diff->h+$diff->i+$diff->s;
+                $marker = $diff->y+$diff->m+$diff->d+$diff->h+$diff->i;
                 if (isset($dataprod['data']) && $marker == 0) {
                     $data[] = $dataprod['data'];
                 } else {
