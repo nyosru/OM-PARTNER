28ca548682164025d18d9ef3d29b40a7899a1e3b
Дмитрий Егоров <desure85@gmail.com>
2015-10-22 13:58:49 +0300

    SEO

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -185,9 +185,9 @@ class SiteController extends Controller
         $d1->setTimestamp(strtotime(trim($checkcache)));
         $d2=new \DateTime();
         $d2->setTimestamp(strtotime(trim($dataque['checkcache'])));
-        $diff = $d2->diff($d1);
-        $markers = $diff->y+$diff->m+$diff->d+$diff->h;
-        if (!isset($dataque['checkcache']) || $markers !== 0 || $diff->i > 5) {
+        $diffs = $d2->diff($d1);
+        $markers = $diffs->y+$diffs->m+$diffs->d+$diffs->h;
+        if (!isset($dataque['checkcache']) || $markers != 0 || $diffs->i > 5) {
 
         if ($searchword == '') {
             $catdataarr = $this->ExtFuncLoad()->categories_for_partners();
@@ -345,7 +345,7 @@ class SiteController extends Controller
         $countfilt = count($data);
         $start = $start_arr;
         Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
-        return array($data, $count_arrs, $price_max, $productattrib, $start, $end_arr, $countfilt, $start_price, $end_price, $prod_attr_query, $page, $sort, $cat_start, $searchword, $type, $hide_man, $chpu, $markers);
+        return array($data, $count_arrs, $price_max, $productattrib, $start, $end_arr, $countfilt, $start_price, $end_price, $prod_attr_query, $page, $sort, $cat_start, $searchword, $type, $hide_man, $chpu, $diff);
     }
 
     public function actionIndex()
