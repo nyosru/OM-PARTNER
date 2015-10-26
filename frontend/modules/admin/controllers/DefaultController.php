c6bbc3f31c3d662df6c517f0c9f274d536c05b01
Дмитрий Егоров <desure85@gmail.com>
2015-10-26 12:27:45 +0300

    Bug Fix

--- frontend/modules/admin/controllers/DefaultController.php
+++ frontend/modules/admin/controllers/DefaultController.php
@@ -70,31 +70,29 @@ class DefaultController extends Controller
         $id = Yii::$app->request->post('id');
         $orders = new PartnersOrders();
         $orders_data = $orders->findOne($id);
-        print_r($orders_data);
-        echo Yii::$app->params['constantapp']['APP_ID'];
-        if (isset($orders_data)) {
-            if (Yii::$app->params['constantapp']['APP_ID'] == $orders_data->partners_id) {
-                if ($orders_data->orders_id == NULL || $orders_data->orders_id == '') {
-                    $orders_data->status = 0;
-                    if ($orders_data->update()) {
-                        $username = User::findOne($orders_data->user_id)->username;
-                        Yii::$app->mailer->compose(['html' => 'order-cancel'], ['order' => $orders_data->order, 'user' => $orders_data->delivery, 'id' => $orders_data->id, 'site' => $_SERVER['HTTP_HOST'], 'site_name' => Yii::$app->params['constantapp']['APP_NAME'], 'date_order' => $orders_data->create_date])
-                            ->setFrom('support@' . $_SERVER['HTTP_HOST'])
-                            ->setTo($username)
-                            ->setSubject('Ваш заказ отменен')
-                            ->send();
+        if (isset($orders_data) && $orders_data !== 9999) {
+                if (Yii::$app->params['constantapp']['APP_ID'] == $orders_data->partners_id) {
+                    if ($orders_data->orders_id == NULL || $orders_data->orders_id == '') {
+                        $orders_data->status = 0;
+                        if ($orders_data->update()) {
+                            $username = User::findOne($orders_data->user_id)->username;
+                            Yii::$app->mailer->compose(['html' => 'order-cancel'], ['order' => $orders_data->order, 'user' => $orders_data->delivery, 'id' => $orders_data->id, 'site' => $_SERVER['HTTP_HOST'], 'site_name' => Yii::$app->params['constantapp']['APP_NAME'], 'date_order' => $orders_data->create_date])
+                                ->setFrom('support@' . $_SERVER['HTTP_HOST'])
+                                ->setTo($username)
+                                ->setSubject('Ваш заказ отменен')
+                                ->send();
+                        } else {
+                            return 'Ошибка обновления статуса заказа';
+                        }
                     } else {
-                        return 'Ошибка обновления статуса заказа';
+                        return 'Заказ уже передан в ОМ';
                     }
                 } else {
-                    return 'Заказ уже передан в ОМ';
+                    return 'Вы не можете редактировать данный заказ';
                 }
             } else {
-                return 'Вы не можете редактировать данный заказ';
+                return 'Нет такого заказа';
             }
-        } else {
-            return 'Нет такого заказа';
-        }
 
     }
 
