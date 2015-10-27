f447c3c3645bfa5000d1504723231f82c19a04d2
desure <desure85@gmail.com>
2015-10-27 22:35:03 +0300

    Change Cache system

--- frontend/themes/default2/views/site/catalog.php
+++ frontend/themes/default2/views/site/catalog.php
@@ -128,7 +128,7 @@ if ($check_ygbot === NULL) { ?>
         new imageLoader(cImageSrc, 'startAnimation()');
         $url = '';
         $url = document.location.hash;
-        if ($url == null) {
+        if ($url == null)
             {
                 $url = '!#cat=932&count=20&start_price=0&end_price=1000000&prod_attr_query=&page=0&sort=10&searchword=';
             }
