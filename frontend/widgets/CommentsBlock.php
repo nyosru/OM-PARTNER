27bcc244e01686b3f8cd19b59361fb1e6809b962
Дмитрий Егоров <desure85@gmail.com>
2015-11-18 12:43:04 +0300

    TrimTags Comments fix length

--- frontend/widgets/CommentsBlock.php
+++ frontend/widgets/CommentsBlock.php
@@ -29,7 +29,7 @@ class CommentsBlock extends \yii\bootstrap\Widget
             foreach ($commentsprovider as $valuecomments) {
                 echo '<div>';
                 echo '<span style=" none repeat scroll 0% 0%; padding: 4px 25px; width: 100%; box-shadow: 2px 1px 5px -4px black;">' . $valuecomments->date_modified . '</span><br/>';
-                $text = $this->trim_tags_text($valuecomments->post, 300);
+                $text = $this->trim_tags_text($valuecomments->post, 400);
                 echo '<span style="padding: 10px 25px; margin: 0px; display: block; none repeat scroll 0% 0%; font-style: italic;">' . $text . '</span>';
                 echo '</div>';
             }
