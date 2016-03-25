18e315a7eb2169a7b44c4d38738ab1fc13ecb531
Desure85 <Oleinum85>
2016-03-25 16:37:13 +0300

    New changelist

--- frontend/controllers/actions/om/ActionPage.php
+++ frontend/controllers/actions/om/ActionPage.php
@@ -2,6 +2,7 @@
 namespace frontend\controllers\actions\om;
 
 use common\models\PartnersPage;
+use yii\bootstrap\Tabs;
 use Faker\Provider\zh_TW\DateTime;
 use Yii;
 use common\models\PartnersConfig;
@@ -10,12 +11,30 @@ trait ActionPage
 {
     public function actionPage()
     {
-        $type = $this->trim_tags_text(Yii::$app->request->post('category', 'page'));
-        $name = $this->trim_tags_text(Yii::$app->request->post('article', 'default'));
+        $name = $this->trim_tags_text(Yii::$app->request->getQueryParam('article'));
+        $page = PartnersPage::find()->where(['partners_id'=>Yii::$app->params['constantapp']['APP_ID'], 'type'=>'post', 'name'=>$name])->one();
+        if(!$page){
+            $page = new PartnersPage();
+        }
+        if(Yii::$app->request->post('PartnersPage')['content']){
 
-        $page = PartnersPage::find()->where(['partners_id'=>Yii::$app->params['constantapp']['APP_ID'], 'type'=>$type, 'name'=>$name])->asArray()->one();
+            $page->content = stripcslashes(Yii::$app->request->post('PartnersPage')['content']);
+            $page->name = $name;
+            $page->type = 'post';
+            $page->active = 1;
+            $page->date_add = 'NULL';
+            $page->date_modify = 'NULL';
+            $page->partners_id = Yii::$app->params['constantapp']['APP_ID'];
+            $page->tags = 'NULL';
+            $page->viewed= 0;
+            $page->validate();
+
+            $page->save();
+        }else{
+
+        }
         $this->layout = 'catalog';
-        return $this->render('page', ['page'=>$page]);
+        return $this->render('page', ['page'=>$page,'error'=>$page->errors]);
 
     }
 }
