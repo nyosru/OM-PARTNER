5232e1fa1830afa019f5bc2b638c097cd8918d21
borodobor <borodobor@gmail.com>
2016-04-04 15:01:05 +0300

    Образ дня

---
+++ common/models/FiguresDays.php
@@ -0,0 +1,24 @@
+<?php
+namespace common\models;
+
+use common\patch\ActiveRecordExt;
+use Yii;
+
+class FiguresDays extends ActiveRecordExt{
+
+    public static function tableName()
+    {
+        return 'partners_figures_days';
+    }
+    
+    public function rules()
+    {
+        return [
+            [['description','image','tags'], 'string' ],
+            [['description','image','tags','date_added'], 'required']
+        ];
+    }
+    public function getProducts(){
+        return $this->hasMany(FiguresDaysProduct::className(),['id'=>'group_id']);
+    }
+}
