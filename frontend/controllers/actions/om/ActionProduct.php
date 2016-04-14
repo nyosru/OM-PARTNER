628f2ebf1488d2b3d99ddeaf740b2b7e776e9d67
borodobor <borodobor@gmail.com>
2016-04-14 16:13:23 +0300

    слайдеры на главной+упаковка в виджет

--- frontend/controllers/actions/om/ActionProduct.php
+++ frontend/controllers/actions/om/ActionProduct.php
@@ -42,6 +42,69 @@ trait ActionProduct
                     if (!$data || ($d1 !== $d2)) {
                         $data = PartnersProductsToCategories::find()->JoinWith('products')->where('products.`products_id` =:id', [':id' => $id])->JoinWith('productsDescription')->JoinWith('productsAttributes')->groupBy(['products.`products_id` DESC'])->JoinWith('productsAttributesDescr')->asArray()->all();
                         $data = end($data);
+                        unset(
+                            $data['old_categories_id'],
+                            $data['products']['country_id'],
+                            $data['products']['date_checked'],
+                            $data['products']['imagenew'],
+                            $data['products']['products_image_lrg'],
+                            $data['products']['products_image_med'],
+                            $data['products']['products_image_sm_1'],
+                            $data['products']['products_image_sm_2'],
+                            $data['products']['products_image_sm_3'],
+                            $data['products']['products_image_sm_4'],
+                            $data['products']['products_image_sm_5'],
+                            $data['products']['products_image_sm_6'],
+                            $data['products']['products_image_xl_1'],
+                            $data['products']['products_image_xl_2'],
+                            $data['products']['products_image_xl_3'],
+                            $data['products']['products_image_xl_4'],
+                            $data['products']['products_image_xl_5'],
+                            $data['products']['products_image_xl_6'],
+                            $data['products']['products_ordered'],
+                            $data['products']['price_coll'],
+                            $data['products']['products_sort_order'],
+                            $data['products']['products_tax_class_id'],
+                            $data['products']['products_to_xml'],
+                            $data['products']['products_weight'],
+                            $data['products']['raschet_pribil'],
+                            $data['products']['removable'],
+                            $data['products']['products_date_available'],
+                            $data['products']['products_date_view'],
+                            $data['productsDescription']['language_id'],
+                            $data['productsDescription']['products_head_desc_tag'],
+                            $data['productsDescription']['products_head_keywords_tag'],
+                            $data['productsDescription']['products_head_title_tag'],
+                            $data['productsDescription']['products_tab_1'],
+                            $data['productsDescription']['products_tab_2'],
+                            $data['productsDescription']['products_tab_3'],
+                            $data['productsDescription']['products_tab_4'],
+                            $data['productsDescription']['products_tab_5'],
+                            $data['productsDescription']['products_tab_6'],
+                            $data['productsDescription']['products_url'],
+                            $data['productsDescription']['products_viewed']
+                        );
+                        foreach($data['productsAttributes'] as $keyattr=>$valueattr){
+                            unset(
+                                $data['productsAttributes'][$keyattr]['options_id'],
+                                $data['productsAttributes'][$keyattr]['options_values_price'],
+                                $data['productsAttributes'][$keyattr]['price_prefix'],
+                                $data['productsAttributes'][$keyattr]['product_attributes_one_time'],
+                                $data['productsAttributes'][$keyattr]['products_attributes_id'],
+                                $data['productsAttributes'][$keyattr]['products_attributes_units'],
+                                $data['productsAttributes'][$keyattr]['products_attributes_units_price'],
+                                $data['productsAttributes'][$keyattr]['products_attributes_weight'],
+                                $data['productsAttributes'][$keyattr]['products_attributes_weight_prefix'],
+                                $data['productsAttributes'][$keyattr]['products_options_sort_order'],
+                                $data['productsAttributes'][$keyattr]['sub_options_values_id']
+                            );
+                        }
+                        foreach($data['productsAttributesDescr'] as $keyattrdesc=>$valueattrdesc){
+                            unset(
+                                $data['productsAttributesDescr'][$keyattrdesc]['language_id'],
+                                $data['productsAttributesDescr'][$keyattrdesc]['products_options_values_thumbnail']
+                            );
+                        }
                         Yii::$app->cache->set($keyprod, ['data' => $data, 'last' =>$checkcache]);
                     } else {
                         $data = $data['data'];
@@ -131,6 +194,69 @@ trait ActionProduct
 
                     $data['products']['products_price'] = intval($data['products']['products_price']) + (intval($data['products']['products_price']) / 100 * intval(Yii::$app->params['partnersset']['discount']['value']));
 
+                }
+                unset(
+                    $data['old_categories_id'],
+                    $data['products']['country_id'],
+                    $data['products']['date_checked'],
+                    $data['products']['imagenew'],
+                    $data['products']['products_image_lrg'],
+                    $data['products']['products_image_med'],
+                    $data['products']['products_image_sm_1'],
+                    $data['products']['products_image_sm_2'],
+                    $data['products']['products_image_sm_3'],
+                    $data['products']['products_image_sm_4'],
+                    $data['products']['products_image_sm_5'],
+                    $data['products']['products_image_sm_6'],
+                    $data['products']['products_image_xl_1'],
+                    $data['products']['products_image_xl_2'],
+                    $data['products']['products_image_xl_3'],
+                    $data['products']['products_image_xl_4'],
+                    $data['products']['products_image_xl_5'],
+                    $data['products']['products_image_xl_6'],
+                    $data['products']['products_ordered'],
+                    $data['products']['price_coll'],
+                    $data['products']['products_sort_order'],
+                    $data['products']['products_tax_class_id'],
+                    $data['products']['products_to_xml'],
+                    $data['products']['products_weight'],
+                    $data['products']['raschet_pribil'],
+                    $data['products']['removable'],
+                    $data['products']['products_date_available'],
+                    $data['products']['products_date_view'],
+                    $data['productsDescription']['language_id'],
+                    $data['productsDescription']['products_head_desc_tag'],
+                    $data['productsDescription']['products_head_keywords_tag'],
+                    $data['productsDescription']['products_head_title_tag'],
+                    $data['productsDescription']['products_tab_1'],
+                    $data['productsDescription']['products_tab_2'],
+                    $data['productsDescription']['products_tab_3'],
+                    $data['productsDescription']['products_tab_4'],
+                    $data['productsDescription']['products_tab_5'],
+                    $data['productsDescription']['products_tab_6'],
+                    $data['productsDescription']['products_url'],
+                    $data['productsDescription']['products_viewed']
+                );
+                foreach($data['productsAttributes'] as $keyattr=>$valueattr){
+                    unset(
+                        $data['productsAttributes'][$keyattr]['options_id'],
+                        $data['productsAttributes'][$keyattr]['options_values_price'],
+                        $data['productsAttributes'][$keyattr]['price_prefix'],
+                        $data['productsAttributes'][$keyattr]['product_attributes_one_time'],
+                        $data['productsAttributes'][$keyattr]['products_attributes_id'],
+                        $data['productsAttributes'][$keyattr]['products_attributes_units'],
+                        $data['productsAttributes'][$keyattr]['products_attributes_units_price'],
+                        $data['productsAttributes'][$keyattr]['products_attributes_weight'],
+                        $data['productsAttributes'][$keyattr]['products_attributes_weight_prefix'],
+                        $data['productsAttributes'][$keyattr]['products_options_sort_order'],
+                        $data['productsAttributes'][$keyattr]['sub_options_values_id']
+                    );
+                }
+                foreach($data['productsAttributesDescr'] as $keyattrdesc=>$valueattrdesc){
+                    unset(
+                        $data['productsAttributesDescr'][$keyattrdesc]['language_id'],
+                        $data['productsAttributesDescr'][$keyattrdesc]['products_options_values_thumbnail']
+                    );
                 }
                 // $catpath = json_decode(file_get_contents('http://' . $_SERVER['HTTP_HOST'] . BASEURL . '/catpath?cat=' . $data['categories_id'] . '&action=namenum'));
                 $list = array();
