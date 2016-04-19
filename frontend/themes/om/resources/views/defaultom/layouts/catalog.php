77cfaa1fe8a650e36a830f2d3106954b4210cc65
Desure85 <Oleinum85>
2016-04-19 13:44:25 +0300

    New changelist

--- frontend/themes/om/resources/views/defaultom/layouts/catalog.php
+++ frontend/themes/om/resources/views/defaultom/layouts/catalog.php
@@ -269,51 +269,7 @@ rmrevin\yii\fontawesome\AssetBundle::register($this);
                     if (isset(Yii::$app->params['partnersset']['yandexcounter']['value']) && Yii::$app->params['partnersset']['yandexcounter']['active'] == 1) {
                         $yandexcounter = Yii::$app->params['partnersset']['yandexcounter']['value'];
                         ?>
-                        <!-- Yandex.Metrika informer -->
-                        <a href="https://metrika.yandex.ru/stat/?id=<?= $yandexcounter ?>&amp;from=informer"
-                           target="_blank" rel="nofollow"><img
-                                src="https://informer.yandex.ru/informer/<?= $yandexcounter ?>/3_1_FFFFFFFF_EFEFEFFF_0_pageviews"
-                                style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика"
-                                title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)"
-                                onclick="try{Ya.Metrika.informer({i:this,id:<?= $yandexcounter ?>,lang:'ru'});return false}catch(e){}"/></a>
-                        <!-- /Yandex.Metrika informer -->
 
-                        <!-- Yandex.Metrika counter -->
-                        <script type="text/javascript">
-                            (function (d, w, c) {
-                                (w[c] = w[c] || []).push(function () {
-                                    try {
-                                        w.yaCounter<?=$yandexcounter?> = new Ya.Metrika({
-                                            id:<?=$yandexcounter?>,
-                                            clickmap: true,
-                                            trackLinks: true,
-                                            accurateTrackBounce: true
-                                        });
-                                    } catch (e) {
-                                    }
-                                });
-
-                                var n = d.getElementsByTagName("script")[0],
-                                    s = d.createElement("script"),
-                                    f = function () {
-                                        n.parentNode.insertBefore(s, n);
-                                    };
-                                s.type = "text/javascript";
-                                s.async = true;
-                                s.src = "https://mc.yandex.ru/metrika/watch.js";
-
-                                if (w.opera == "[object Opera]") {
-                                    d.addEventListener("DOMContentLoaded", f, false);
-                                } else {
-                                    f();
-                                }
-                            })(document, window, "yandex_metrika_callbacks");
-                        </script>
-                        <noscript>
-                            <div><img src="https://mc.yandex.ru/watch/<?= $yandexcounter ?>"
-                                      style="position:absolute; left:-9999px;" alt=""/></div>
-                        </noscript>
-                        <!-- /Yandex.Metrika counter -->
                     <? } ?>
                 </div>
 <!--                <p class="pull-right"><a href="--><?//= BASEURL ?><!--/offerta">Оферта</a> <a-->
@@ -331,7 +287,9 @@ rmrevin\yii\fontawesome\AssetBundle::register($this);
         ?>
 </div>
 
+<script>
 
+    </script>
     <script type="text/javascript">
         $(document).ready(function(){
             $('.target').shortscroll();
@@ -353,6 +311,34 @@ rmrevin\yii\fontawesome\AssetBundle::register($this);
         ga('send', 'pageview');
         ga('require', 'ec');
 
+    </script>
+    <script type="text/javascript">
+        (function (d, w, c) {
+            (w[c] = w[c] || []).push(function() {
+                try {
+                    w.yaCounter36825930 = new Ya.Metrika({
+                        id:36825930,
+                        clickmap:true,
+                        trackLinks:true,
+                        accurateTrackBounce:true,
+                        webvisor:true,
+                        trackHash:true,
+                        ecommerce:"container-fluid float"
+                    });
+                } catch(e) { }
+            });
+
+            var n = d.getElementsByTagName("script")[0],
+                s = d.createElement("script"),
+                f = function () { n.parentNode.insertBefore(s, n); };
+            s.type = "text/javascript";
+            s.async = true;
+            s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+            if (w.opera == "[object Opera]") {
+                d.addEventListener("DOMContentLoaded", f, false);
+            } else { f(); }
+        })(document, window, "yandex_metrika_callbacks");
     </script>
     </body>
     </html>
