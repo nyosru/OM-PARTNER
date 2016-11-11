1b3853b05426cec8db5bdc4aae74c8e1b49475a2
desure85 <desure85@gmail.com>
2016-11-11 14:33:03 +0300

    fixSelectedProduct

--- frontend/themes/om/resources/views/defaultom/site/selectedproduct.php
+++ frontend/themes/om/resources/views/defaultom/site/selectedproduct.php
@@ -19,29 +19,35 @@ $this -> title = 'Избранные продукты';
                 var mandata = [];
                 var requestdata = [];
 
-                    requestdata = $.ajax({
-                        method: 'post',
-                        url: "/site/selectedproduct",
-                        async: false,
-                        data: {products:$i},
-                        success: function (data) {
-                            $bside = $('.bside').html();
-                            $('.bside').html('');
-                            $.each(data, function(i,item){
+                requestdata = $.ajax({
+                    method: 'post',
+                    url: "/site/selectedproduct",
+                    async: false,
+                    data: {products:$i},
+                    success: function (data) {
+                        $bside = $('.bside').html();
+                        $('.bside').html('');
+                        $.each(data, function(i,item){
 
-                                if(getCookie('cardview')==1) {
-                                    renderProduct2(item.products, item.productsDescription,item.productsAttributes, item['productsAttributesDescr'], '',item.catpath);
-                                }else{
-                                     renderProduct(item.products, item.productsDescription, item.productsAttributes, item['productsAttributesDescr'], '',item.catpath);
-                                }
-                            });
-                            $('.bside').append($bside);
-                        }
-                    });
+                            if(getCookie('cardview')==1) {
+                                renderProduct2(item.products, item.productsDescription,item.productsAttributes, item['productsAttributesDescr'], '',item.catpath);
+                                $('[itemid = "'+item.products['products_id']+'"]').prepend('<div class="del-products" style="top: 5px; right: 10px; float: right; cursor: pointer; color: red; font-size: 25px; position: relative;"><i  class="fa fa-times"></i></div>');
+                            }else{
+                                console.log(item);
+                                renderProduct(item.products, item.productsDescription, item.productsAttributes, item['productsAttributesDescr'], '',item.catpath);
+                                $('[itemid = "'+item.products['products_id']+'"]').prepend('<div class="del-products" style="top: 5px; right: 10px; float: right; position: absolute; cursor: pointer; color: red; font-size: 25px;"><i  class="fa fa-times"></i></div>');
+
+                            }
+
+
+                        });
+                        $('.bside').append($bside);
+                    }
+                });
 
 
 
-                    
+
 //
 
             }
