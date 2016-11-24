957fb0842c0c78cbe71368e4242a67f6c98ec1a5
desure85 <desure85@gmail.com>
2016-11-24 14:42:47 +0300

    Отложенная загрузка слизы

--- frontend/widgets/SlizaWidget.php
+++ frontend/widgets/SlizaWidget.php
@@ -12,11 +12,26 @@ class SlizaWidget extends \yii\bootstrap\Widget
 
         ?>
 
-        <!-- Sliza.ru - Widget -->
-        <script type="text/javascript" src="https://sliza.ru/widget.php?id=842&h=2a7bebeb9808e01a869c9f734db908e3&t=s"
-                async defer></script>
-        <!-- /// -->
-
+        <script>
+            $(window).on('load', function(){
+                var script = document.createElement('script');
+                script.src = "https://sliza.ru/widget.php?id=842&h=2a7bebeb9808e01a869c9f734db908e3&t=s";
+                document.documentElement.appendChild(script);
+                script.onload = script.onerror = function() {
+                    if (!this.executed) { // выполнится только один раз
+                        this.executed = true;
+                    }
+                };
+                script.onreadystatechange = function() {
+                    var self = this;
+                    if (this.readyState == "complete" || this.readyState == "loaded") {
+                        setTimeout(function() {
+                            self.onload()
+                        }, 0);
+                    }
+                };
+            });
+        </script>
         <?php
     }
 
