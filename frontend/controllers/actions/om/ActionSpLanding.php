cbf3eb61152177f3894d7f304385b1eae3f6ca53
Евгений Ниценко <nitsenko94@gmail.com>
2016-12-03 21:30:01 +0200

    [cs] Автоформатирование ActionSpLanding.php

--- frontend/controllers/actions/om/ActionSpLanding.php
+++ frontend/controllers/actions/om/ActionSpLanding.php
@@ -17,32 +17,34 @@ trait ActionSpLanding
 
         $this->layout = 'lp';
         $model = new InviteSPForm();
-        if($model->load(Yii::$app->request->post()) && $model->validate()){
-            if($model->InviteSp()){
-                Yii::$app->params['params']['products_mail'] =  $this->NewProducts(6,'mail_new-34', 7200);
-                Yii::$app->params['params']['utm'] =  [
-                    'source'=>'newom',
-                    'medium'=>'email',
-                    'campaign'=>'om',
-                    'content'=>'invite-sp'
+        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
+            if ($model->InviteSp()) {
+                Yii::$app->params['params']['products_mail'] = $this->NewProducts(6, 'mail_new-34', 7200);
+                Yii::$app->params['params']['utm'] = [
+                    'source'   => 'newom',
+                    'medium'   => 'email',
+                    'campaign' => 'om',
+                    'content'  => 'invite-sp',
                 ];
                 Yii::$app->mailer->htmlLayout = 'layouts-om/html';
                 Yii::$app->mailer->compose('invite-sp')
                     ->setFrom('odezhdamaster@gmail.com')
                     ->setTo($model->email)
-                    ->setSubject('Приглашение в сервис  '  . $_SERVER['HTTP_HOST'])
-                    ->send();
+                    ->setSubject('Приглашение в сервис  ' . $_SERVER['HTTP_HOST'])
+                    ->send()
+                ;
                 \Yii::$app->getSession()->setFlash('success', 'Успешно отправлено');
 
-                return $this->redirect(['sp-landing', ['model'=> $model]]);
+                return $this->redirect(['sp-landing', ['model' => $model]]);
             }
         }
-        if($model->errors){
-            foreach ($model->errors as $err){
+        if ($model->errors) {
+            foreach ($model->errors as $err) {
                 \Yii::$app->getSession()->setFlash('error', $err);
             }
         }
-        return $this->render('sp', ['model'=>$model]);
+
+        return $this->render('sp', ['model' => $model]);
     }
 
 }
