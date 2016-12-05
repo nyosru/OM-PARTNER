e0046cbd9a90e5577d702a3e2135b811acf41cf7
desure85@gmail.com <Oleinum85>
2016-12-05 14:03:31 +0300

    Fixes

--- common/forms/PartnersOrders/CommentForm.php
+++ common/forms/PartnersOrders/CommentForm.php
@@ -27,9 +27,9 @@ class CommentForm extends Model
     {
         return [
             [['attr','order','id'], 'integer'],
-            [['attr','order','id'], 'required'],
+            [['order'], 'required'],
             ['comment', 'string'],
-            ['comment', 'required']
+            ['comment', 'required',  'message' => 'Необходимо заполнить']
         ];
     }
 
@@ -50,16 +50,16 @@ class CommentForm extends Model
                 ->asArray()->one()) == TRUE)
         {
             foreach (unserialize($order_data['order'])['products'] as $key=>$value){
-                if($value[2] == $this->attr && $value[0] == $this->id){
-                    if(isset($value['comment']) &&  $value['comment'] != FALSE){
-                        $comment = $value['comment'];
+                if(($value[2] == $this->attr || !$value[2]) && $value[0] == $this->id){
+                    if(isset($value[8]['comment']) &&  $value[8]['comment'] != FALSE){
+                        $this->comment = $value[8]['comment'];
                     }else{
-                        $comment = '';
+                        $this->comment = '';
                     }
                     break;
                 }
             }
-            return $comment;
+            return true;
         }else{
             return $this->addError('order', 'Заказ не найден');
         }
@@ -83,20 +83,51 @@ class CommentForm extends Model
         {
             $new_value = unserialize($order_data->order);
             foreach ($new_value['products'] as $key=>$value){
-                if($value[2] == $this->attr && $value[0] == $this->id){
-                    $value['comment'] = $this->comment;
+                if(($value[2] == $this->attr || !$value[2]) && $value[0] == $this->id){
+                    $value[8]['comment'] = $this->comment;
                 }
                 $new_value['products'][$key] = $value;
             }
-            $order_data->order = $new_value;
-            if($order_data->save()){
-                return $order_data->errors;
+            $order_data->order = serialize($new_value);
+            if($order_data->validate() && $order_data->save()){
+                return true;
             }else{
-                return $this->addError('order', 'Заказ не сохранен');
+                return $this->addError('comment', 'Заказ не сохранен');
             }
         }else{
-            return $this->addError('order', 'Заказ не найден');
+            return $this->addError('comment', 'Заказ не найден 23');
         }
     }
+
+    public function saveOrderComment()
+    {
+        if(
+            ($referral = Referrals::find()
+                ->where('user_id = :user',[':user'=>Yii::$app->user->getId()])
+                ->asArray()
+                ->one()) == TRUE
+            &&
+            ($order_data = PartnersOrders::find()
+                ->where(PartnersOrders::tableName().'.id = :id',
+                    [':id'=>$this->order])
+                ->andWhere('referral_id = :ref',
+                    [':ref'=>$referral['id']])
+                ->joinWith('referralUser')
+                ->one()) == TRUE)
+        {
+            $new_value = unserialize($order_data->order);
+            $new_value['comment'] = $this->comment;
+            $order_data->order = serialize($new_value);
+            if($order_data->validate() && $order_data->save()){
+                return true;
+            }else{
+                return $this->addError('comment', 'Заказ не сохранен');
+            }
+        }else{
+            return $this->addError('comment', 'Заказ не найден 23');
+        }
+    }
+
+
 
 }
