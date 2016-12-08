7eb4eae101eb424cd73e3ffb31ca474720abdf79
desure85@gmail.com <Oleinum85>
2016-12-08 13:07:47 +0300

    Fix Merge

--- frontend/themes/om/resources/views/omdesign/site/allcategories.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: dmitriy
- * Date: 13.05.16
- * Time: 10:47
- */
-
-echo \frontend\widgets\Menuom::widget(['property' => ['id' => 'all-categories', 'target' => '0', []]]);;
