21a928d8879c75996d531f31951c38b048a2b6b6
desure85@gmail.com <Oleinum85>
2016-12-13 16:01:16 +0300

    Del View analytics

--- frontend/themes/om/resources/views/defaultom/site/cataloggibridmulty.php
+++ frontend/themes/om/resources/views/defaultom/site/cataloggibridmulty.php
@@ -290,40 +290,14 @@ if ($data[0] != 'Не найдено!') {
     $analitics = '';
     if($_COOKIE['cardview']==1){
         foreach ($data[0] as $key=>$value) {
-            $analitics .= '
-       <script>
-       ga("ec:addProduct", {              
-            "id": "'.$value['products']['products_id'].'",   
-            "name": "'.htmlentities($value['productsDescription']['products_name']).'", 
-            "category": "'.implode('/',$catpath['name']).'",    
-            "list": "main-catalog",                
-            "brand": "'.$value['products']['manufacturers_id'].'",                
-            "variant": "none",                 
-            "position": "'.$key.'"});
-        ga("ec:setAction", "view");
-        ga("send", "event" , "view", "'.$_SERVER["REQUEST_URI"].'" );
-        </script>
-        ';
+            $analitics .= '';
             $spec = $value['productsSpecification']['74']['specification_values_id'];
             $spec_code = $value['specificationValuesDescription'][$spec]['specification_value'];
             $products .= ProductCard2::widget(['product'=>$value['products'],'description'=>$value['productsDescription'],'attrib'=>$value['productsAttributes'],'attr_descr'=>$value['productsAttributesDescr'],'catpath'=>$catpath, 'man_time'=>$man_time, 'category'=>$value['categories_id'], 'showdiscount'=>1, 'season'=>$spec_code]);
         }
     }else{
         foreach ($data[0] as $key=>$value) {
-            $analitics .= '
-       <script>
-       ga("ec:addProduct", {              
-            "id": "'.$value['products']['products_id'].'",   
-            "name": "'.htmlentities($value['productsDescription']['products_name']).'", 
-            "category": "'.implode('/',$catpath['name']).'",     
-            "list": "main-catalog",                
-            "brand": "'.$value['products']['manufacturers_id'].'",                
-            "variant": "none",                 
-            "position": "'.$key.'"});
-        ga("ec:setAction", "view");
-        ga("send", "event" , "view", "'.$_SERVER["REQUEST_URI"].'" );
-        </script>
-        ';
+            $analitics .= '';
             $spec = $value['productsSpecification']['74']['specification_values_id'];
             $brand = $value['productsSpecification']['77']['specification_values_id'];
             $spec_code = $value['specificationValuesDescription'][$spec]['specification_value'];
