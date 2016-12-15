1fe44cdff7ca12af492d8a5e0dfa5df364297fe9
Nitsenko <nitsenko94@gmail.com>
2016-12-15 14:40:35 +0300

    [task] Убрал неиспользуемые неймспейсы.

--- frontend/modules/sp/controllers/actions/ActionAddCommon.php
+++ frontend/modules/sp/controllers/actions/ActionAddCommon.php
@@ -2,16 +2,7 @@
 namespace frontend\modules\sp\controllers\actions;
 
 use common\forms\PartnersOrders\CommonOrderForm;
-use common\models\CommonOrders;
-use common\models\PartnersOrders;
-use common\models\Referrals;
-use common\models\ReferralsUser;
 use Yii;
-use yii\base\Exception;
-use yii\data\ActiveDataProvider;
-use yii\data\Sort;
-use yii\validators\DateValidator;
-use yii\web\HttpException;
 
 
 trait ActionAddCommon
