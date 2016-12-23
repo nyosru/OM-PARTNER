96d6ce619b01d44dbabcbf372b389157b5737d04
Nitsenko <nitsenko94@gmail.com>
2016-12-23 16:27:54 +0300

    [fix] Изменена проверка, на правильную по артикулу.

--- common/traits/Orders/UpdateOrder.php
+++ common/traits/Orders/UpdateOrder.php
@@ -7,7 +7,7 @@ class UpdateOrder
 {
     /**
      * @param PartnersOrders $order
-     * @param $client_order_products
+     * @param array $client_order_products
      * @return PartnersOrders
      */
     public function updateOrderWithClientProducts(PartnersOrders $order, $client_order_products)
@@ -18,15 +18,15 @@ class UpdateOrder
         }
 
         $un_order = unserialize($order->order);
+
         foreach ($un_order['products'] as $key_back => &$product_back) {
             foreach ($client_order_products as $key_client => $product_client) {
 
-                if ($product_back[0] == $product_client[0] && $product_back[2] == $product_client[2]) {
+                if ($product_back[0] == $product_client[0] && $product_back[1] == $product_client[1]) {
                     $un_order['products'][$key_back][4] = ((int)$product_client[4] >= 0) ? $product_client[4] : 0;
                 }
             }
         }
-
         $products_after_deleting = array_filter($un_order['products'],
             function ($element) use ($client_order_products) {
                 return in_array($element, $client_order_products);
