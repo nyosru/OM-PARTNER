2454687c20f8513139551acc24e3520cc3cc2585
Nitsenko <nitsenko94@gmail.com>
2016-12-26 22:36:42 +0300

    [task] Добавил ActionPreviewLand.

--- frontend/modules/cat/controllers/v0/DefaultController.php
+++ frontend/modules/cat/controllers/v0/DefaultController.php
@@ -3,12 +3,14 @@
 namespace frontend\modules\cat\controllers\v0;
 
 use frontend\modules\cat\controllers\actions\ActionIndex;
+use frontend\modules\cat\controllers\actions\ActionPreviewLand;
 use Yii;
 use yii\web\Controller;
 
 class DefaultController extends Controller
 {
     use ActionIndex;
+    use ActionPreviewLand;
 
     public function behaviors()
     {
