691bb874fa77c9050fd9b051aeb65df6f421a3e1
Nitsenko <nitsenko94@gmail.com>
2016-12-26 22:39:42 +0300

    [task] Добавил больше параметров в форму и сохранения конфигураций.

--- common/forms/Cat/CatLandConfigForm.php
+++ common/forms/Cat/CatLandConfigForm.php
@@ -9,13 +9,29 @@ use yii\base\Model;
 class CatLandConfigForm extends Model
 {
     public $header_tpl;
+    public $header_title;
+
     public $content_tpl;
+    public $content_list_products;
+    public $special_offer;
+
     public $footer_tpl;
 
     public function rules()
     {
         return [
-            [['header_tpl', 'content_tpl', 'footer_tpl'], 'string'],
+            [
+                [
+                    'header_tpl',
+                    'header_title',
+                    'content_tpl',
+                    'content_list_products',
+                    'special_offer',
+                    'footer_tpl',
+                ],
+                'string'
+            ],
+
         ];
     }
 
@@ -25,7 +41,23 @@ class CatLandConfigForm extends Model
             return false;
         }
 
-        return false;
+        $json_config = [
+          "header_tpl" => $this->header_tpl,
+          "header_config" => [
+              "header_title" => $this->header_title
+          ],
+          "content_tpl" => $this->header_tpl,
+          "content_config" => [
+            "content_list_products" => $this->content_list_products,
+            "special_offer"=> $this->special_offer,
+          ],
+          "footer_tpl" => $this->footer_tpl,
+        ];
+
+        if(file_put_contents(Yii::getAlias('@frontend') .'/runtime/cat/config.json', json_encode($json_config))) {
+            return true;
+        }
+        return true;
     }
 
 }
