aa0018e1ceddb130aea13782698fb0c897ffc4e2
Nitsenko <nitsenko94@gmail.com>
2017-01-10 00:57:39 +0300

    [task] Теперь можно получать конфиг по параметрам и создавать новый, если ничего не приходит.

--- frontend/modules/cat/controllers/actions/ActionIndex.php
+++ frontend/modules/cat/controllers/actions/ActionIndex.php
@@ -13,25 +13,24 @@ trait ActionIndex
         $preview_toggle = Yii::$app->request->get('preview_toggle');
         $config_name = Yii::$app->request->get('c');
 
-        $scandir_cat_configs = array_diff(scandir(\Yii::getAlias('@runtime') . '/cat/'), ['..', '.']);
-        $scandir_cat_configs = array_values($scandir_cat_configs);
-        if($preview_toggle) {
-
+        if ($preview_toggle) {
             $j = file_get_contents(Yii::getAlias('@frontend') . '/runtime/cat/preview_config.json');
         } else {
-            if(in_array($config_name, $scandir_cat_configs)) {
-                $exist_config_name = $config_name;
-            } else {
-                $exist_config_name = $scandir_cat_configs[0];
+            if ($config_name != 'preview_config') {
+                $j = file_get_contents(Yii::getAlias('@frontend') . '/runtime/cat/' . $config_name);
             }
-            $j = file_get_contents(Yii::getAlias('@frontend') . '/runtime/cat/'. $exist_config_name);
         }
-        $land_config = (array)json_decode($j, true);
+
+        $land_config = [];
+        if (isset($j) && $j) {
+            $land_config = (array)json_decode($j, true);
+        }
 
         $model = new CatLandConfigForm();
 
-        if(!empty($land_config) && count($land_config) > 0) {
+        if (!empty($land_config) && count($land_config) > 0) {
             $model = new CatLandConfigForm([
+                'config_name'           => $config_name,
                 'header_tpl'            => $land_config['header_tpl'],
                 'header_title'          => $land_config['header_config']['header_title'],
                 'content_tpl'           => $land_config['content_tpl'],
