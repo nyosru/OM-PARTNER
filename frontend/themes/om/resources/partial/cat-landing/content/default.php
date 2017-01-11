8f51d99d74b1e5542ed5f833b4301cb1e47eda90
Nitsenko <nitsenko94@gmail.com>
2017-01-11 10:41:49 +0300

    [task] Корекция html и сменил id в owl

--- frontend/themes/om/resources/partial/cat-landing/content/default.php
+++ frontend/themes/om/resources/partial/cat-landing/content/default.php
@@ -2,7 +2,7 @@
     <h1>Отобранные товары</h1>
 </div>
 
-<div id="lend_products" style="">
+<div id="lend_products">
     <div class="row-e">
         <div class="col-9-10 center">
             <div id="owl_lend_products_product-card" class="owl-carousel owl-theme">
@@ -19,29 +19,29 @@
             </div>
         </div>
     </div>
+</div>
 
-    <div class="special_header">
-        <h1>Специальное предложение на сегодня!</h1>
-    </div>
+<div class="special_header">
+    <h1>Специальное предложение на сегодня!</h1>
+</div>
 
-    <div class="row-e" style="box-shadow: 0px 3px 6px -2px rgba(0,0,0,0.75);">
-        <div class="col-1-3">
-            <div class="img-block">
-                <img src="http://remdta.ru/des/specpredlozhenie.png" alt="">
-            </div>
-        </div>
-        <div class="col-2-3">
-            <div class="offer-body" style="padding-right: 25px">
-                <?= $special_offer ?>
-            </div>
+<div class="row-e" style="box-shadow: 0px 3px 6px -2px rgba(0,0,0,0.75);">
+    <div class="col-1-3">
+        <div class="img-block">
+            <img src="http://remdta.ru/des/specpredlozhenie.png" alt="">
         </div>
     </div>
+    <div class="col-2-3">
+        <div class="offer-body" style="padding-right: 25px">
+            <?= $special_offer ?>
+        </div>
+    </div>
+</div>
 
 <?php
 $script = <<< JS
-    $(document).ready(function () {
 
-        $("#owl_lend_products").owlCarousel({
+        $("#owl_lend_products_product-card").owlCarousel({
           items : 4, //10 изображений на 1000px 
           itemsDesktop : [1000,4], //5 изображений на ширину между 1000px и 901px
           itemsDesktopSmall : [900,3], // 3 изображения между 900px и 601px
@@ -50,7 +50,6 @@ $script = <<< JS
           navigation : true
         });
 
-    });
 JS;
 $this->registerJs($script, yii\web\View::POS_END);
 ?>
