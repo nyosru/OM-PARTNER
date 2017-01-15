01a14d3bffbdffe8d8abcd8f811e6a69b4ab9861
Nitsenko <nitsenko94@gmail.com>
2017-01-16 00:50:10 +0300

    [task] Добавил карзину и виджет для баннера.

--- frontend/themes/om/resources/partial/cat-landing/header/default.php
+++ frontend/themes/om/resources/partial/cat-landing/header/default.php
@@ -1,7 +1,13 @@
 <div class="row-e">
     <div class="col-1">
         <div class="header_bar" style="height: 55px;border-bottom: 1px solid rgb(204, 204, 204);">
-            <div class="top-link-cont" style="padding: 12px 9px; float: right; text-align: right;"><div style="background: rgb(255, 191, 8) none repeat scroll 0% 0%; font-size: 12px; float: right; position: relative; right: 35px;" class="cart-count badge">5</div><a rel="nofollow" class="top-link" href="/glavnaya/cart"><i class="fa fa-shopping-cart" style="font-size: 28px; color: rgb(0, 165, 161);"></i></a></div>
+            <div class="top-link-cont" style="padding: 12px 9px; float: right; text-align: right;">
+                <div style="background: rgb(255, 191, 8) none repeat scroll 0% 0%; font-size: 12px; float: right; position: relative; right: 35px;"
+                     class="cart-count badge">5
+                </div>
+                <a rel="nofollow" class="top-link" href="/glavnaya/cart"><i class="fa fa-shopping-cart"
+                                                                            style="font-size: 28px; color: rgb(0, 165, 161);"></i></a>
+            </div>
             <div class="header_bar_img">
                 <img src="/images/logo/OM_code.png">
             </div>
@@ -12,39 +18,23 @@
 <div class="special_header">
     <h1><?= ($header_config) ?: 'Специальное предложение' ?></h1>
 </div>
-<div id="banners">
-    <div class="row-e">
-        <div class="col-1">
-            <div class="row-e" style="margin-top: 10px">
-                <div class="col-1-4">
-                    <img style="display: block;max-width: 100%;height: auto; margin-bottom: 5px"
-                         src="/images/banners/OM_14122016_1.png" alt="Нарядный образ на работу">
-                    <img style="display: block;max-width: 100%;height: auto; " src="/images/banners/OM_14122016_4.png"
-                         alt="Наряд для маленькой принцессы">
-                </div>
-                <div class="col-1-4">
-                    <img style="display: block;max-width: 100%;height: auto; margin-bottom: 9px"
-                         src="/images/banners/OM_14122016_3.png" alt="Теплые колготки для зимнего образа">
-                    <img style="display: block;max-width: 100%;height: auto; " src="/images/banners/OM_14122016_2.png"
-                         alt="Уютные кофточки">
-                </div>
-                <div class="col-1-2">
-                    <img style="display: block;max-width: 100%;height: auto;" src="/images/banners/OM_14122016_5.png"
-                         alt="В новый год на корпоратив">
-                </div>
-            </div>
-        </div>
-    </div>
-<?php //echo \frontend\widgets\MainBanner::widget(); ?>
-</div>
 
-<div class="row-e"  style="margin-top: 25px">
+<!-- banner -->
+<?= \frontend\widgets\MainBanner::widget($main_banner_config); ?>
+<div class="clearfix"></div>
+<!-- end banner -->
+
+<!--footer banner -->
+<div class="row-e" style="margin-top: 25px">
     <div class="col-1">
         <a href="https://www.instagram.com/odezhda_master/" target="_blank"><img
                     style="display: block;max-width: 100%;height: auto;" src="/images/banners/OM_14122016_6.png"
                     alt="Топ 5 лучших товаров каждый день"></a>
     </div>
 </div>
+<!-- end banner -->
+
+
 
 <div id="cat-products">
     <div class="row-e">
@@ -94,6 +84,7 @@
                     <div class="row-e">
                         <div class="col-1-3">
                             <img width="100%" src="/images/logo/minorder.png">
+
                         </div>
                         <div class="col-2-3 about_text">
                             Минимальный заказ всего от 5000 рублей
@@ -102,11 +93,5 @@
                 </div>
             </div>
         </div>
-    </div>
-</div>
-
-<div id="special-offer">
-    <div class="col-1-8">
-
     </div>
 </div>
