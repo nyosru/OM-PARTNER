6aefe1a420a1e14c1b9aaa7818a3df17f45a8820
Nitsenko <nitsenko94@gmail.com>
2017-01-18 16:24:28 +0300

    [task] Добавил к выходным параметрам в форму, дополненый конфиг с картинками

--- frontend/modules/adminsite/controllers/actions/cat/ActionConfigure.php
+++ frontend/modules/adminsite/controllers/actions/cat/ActionConfigure.php
@@ -2,6 +2,7 @@
 namespace frontend\modules\adminsite\controllers\actions\cat;
 
 use common\forms\Cat\CatLandConfigForm;
+use common\models\Referrals;
 use Yii;
 
 trait ActionConfigure
@@ -17,7 +18,7 @@ trait ActionConfigure
             $j = file_get_contents(Yii::getAlias('@frontend') . '/runtime/cat/preview_config.json');
         } else {
             if ($config_name != 'preview_config') {
-                $j = file_get_contents(Yii::getAlias('@frontend') . '/runtime/cat/' . $config_name.'.json');
+                $j = file_get_contents(Yii::getAlias('@frontend') . '/runtime/cat/' . $config_name . '.json');
             }
         }
 
@@ -33,14 +34,16 @@ trait ActionConfigure
                 'config_name'           => $config_name,
                 'header_tpl'            => $land_config['header_tpl'],
                 'header_title'          => $land_config['header_config']['header_title'],
+                'banners_tpl'           => $land_config['header_config']['banner_config']['template'],
+                'images_cfg'            => json_encode($land_config['header_config']['banner_config']['images']),
                 'content_tpl'           => $land_config['content_tpl'],
                 'content_list_products' => $land_config['content_config']['content_list_products'],
                 'special_offer'         => $land_config['content_config']['special_offer'],
                 'footer_tpl'            => $land_config['footer_tpl'],
             ]);
         }
-
-        return $this->render('cat/configure/index', ['model' => $model, 'land_config' => $land_config]);
+        return $this->render('cat/configure/index',
+            ['model' => $model, 'land_config' => $land_config]);
     }
 
 }
