435e7eef92db61268ff7a5e6648195fc4a3a11ca
desure85@gmail.com <Oleinum85>
2017-01-18 17:25:15 +0300

    Metagen upd

--- common/traits/PageGen/MetaGen.php
+++ common/traits/PageGen/MetaGen.php
@@ -5,6 +5,7 @@ use common\models\Featured;
 use common\models\PartnersProductsToCategories;
 use common\traits\Categories\CustomCatalog;
 use Yii;
+use yii\helpers\ArrayHelper;
 use yii\web\View;
 
 
@@ -20,45 +21,46 @@ trait MetaGen
      */
     static function metaGen($view = '' , $version = '', $type = '',$params = '')
     {
-        if (isset(Yii::$app->params['metagen']) && Yii::$app->params['metagen'] == TRUE) {
-            $h1 = TRUE;
-            if (isset($version)) {
-                $templates = [];
-                if ($type == 'catalog') {
-                    $templates = self::customCatalog()['themes'];
-                    $version = trim(self::customCatalog()['template'][$params]['name']);
-                }
-                if (key_exists($version, $templates)) {
-                    $pattern = [];
-                    $replace = [];
-                    $templates = $templates[$version];
-                    if ($type == 'catalog') {
-                        $params = self::customCatalog()['template'][$params];
-                        foreach ($params['param'] as $key => $value) {
+
+        $h1 = TRUE;
+        if (isset($version)) {
+            $templates = [];
+            if($type == 'catalog'){
+                $templates = self::customCatalog()['themes'];
+                $version = trim(self::customCatalog()['template'][$params]['name']);
+            }
+            if (key_exists($version, $templates)) {
+                $pattern = [];
+                $replace = [];
+                $templates = $templates[$version];
+                if($type == 'catalog'){
+                    $param = self::customCatalog()['template'][$params];
+                    foreach ($param['param'] as $key=>$value){
+                        if($value) {
                             $pattern[] = '/({\$' . $key . '})/iu';
                             $replace[] = str_replace('"', '', $value);
                         }
                     }
-                    if (isset($templates['description'])) {
-                        $view->registerMetaTag(['name' => 'description', 'content' => preg_replace($pattern, $replace, $templates['description'])]);
-                    }
-                    if (isset($templates['keywords'])) {
-                        $view->registerMetaTag(['name' => 'keywords', 'content' => preg_replace($pattern, $replace, $templates['keywords'])]);
-                    }
-                    if (isset($templates['title'])) {
-                        $view->title = preg_replace($pattern, $replace, $templates['title']);
-                    }
-                    if (isset($templates['h1'])) {
-                        $h1 = preg_replace($pattern, $replace, $templates['h1']);
-                    }
-                    return $h1;
-                } else {
-                    return FALSE;
+                    $pattern[] = '/({\$cat})/iu';
+                    $replace[] = ArrayHelper::index(self::categories_for_partners()[1], 'categories_id')[$params]['categories_name'];
                 }
+                if(isset($templates['description'])){
+                    $view->registerMetaTag(['name' => 'description', 'content' => preg_replace($pattern,$replace,$templates['description'])]);
+                }
+                if(isset($templates['keywords'])){
+                    $view->registerMetaTag(['name' => 'keywords', 'content' =>  preg_replace($pattern,$replace,$templates['keywords'])]);
+                }
+                if(isset($templates['title'])){
+                        $view->title =  preg_replace($pattern,$replace,$templates['title']);
+                }
+                if(isset($templates['h1'])){
+                    $h1 = preg_replace($pattern,$replace,$templates['h1']);
+                }
+                return $h1;
             } else {
                 return FALSE;
             }
-        }else{
+        } else {
             return FALSE;
         }
     }
