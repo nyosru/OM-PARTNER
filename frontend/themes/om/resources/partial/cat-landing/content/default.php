54937f57f55dd0dde38665c5c92880447aad5ee7
Nitsenko <nitsenko94@gmail.com>
2017-01-19 03:48:47 +0300

    [task] Переделан content tpl

--- frontend/themes/om/resources/partial/cat-landing/content/default.php
+++ frontend/themes/om/resources/partial/cat-landing/content/default.php
@@ -1,49 +1,67 @@
-<div class="special_header">
-    <h1>Отобранные товары</h1>
-</div>
+<div class="o-container col-95">
 
-<div id="lend_products">
-    <div class="row-e">
-        <div class="col-9-10 center">
-            <div id="owl_lend_products_product-card" class="owl-carousel owl-theme">
-                <?php foreach ($content_list_products as $value) : ?>
-                    <div class="item">
-                        <?= \frontend\widgets\ProductCard::widget([
-                            'product'     => $value,
-                            'description' => $value['productsDescription'],
-                            'attrib'      => $value['productsAttributes'],
-                            'attr_descr'  => $value['productsAttributesDescr'],
-                        ]); ?>
-                    </div>
-                <?php endforeach; ?>
+    <div id="lend_products">
+        <div class="row-e">
+            <div class="col-1 center">
+                <div id="owl_lend_products_product-card" class="owl-carousel owl-theme">
+                    <?php foreach ($content_list_products as $value) : ?>
+                        <div class="item">
+                            <?= \frontend\widgets\ProductCard::widget([
+                                'product'     => $value,
+                                'description' => $value['productsDescription'],
+                                'attrib'      => $value['productsAttributes'],
+                                'attr_descr'  => $value['productsAttributesDescr'],
+                            ]); ?>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
             </div>
         </div>
     </div>
-</div>
 
-<div class="row-e" style="">
-    <div class="col-1-3">
-        <div class="img-block">
-            <img src="http://remdta.ru/des/specpredlozhenie.png" alt="">
+    <div class="cat-info-about-us">
+        <div class="cat-info-about-us_b_h3">
+            <h3>С нами работают и зарабатывают более 34464 клиентов по всей России и СНГ</h3>
+        </div>
+        <div class="cat-info-about-us_btn">
+            <div class="cat-info-about-us_btn__btn">
+                <span>Подробные условия работы и частные вопросы</span>
+            </div>
         </div>
     </div>
-    <div class="col-2-3">
-        <div class="offer-body" style="padding-right: 25px">
+
+
+<!--    <div class="cat_customer_reviews">-->
+<!--        <div class="row-e">-->
+<!--            <div class="col-1-2">-->
+<!--                123-->
+<!--            </div>-->
+<!--            <div class="col-1-2">-->
+<!--                123-->
+<!--            </div>-->
+<!--        </div>-->
+<!--        <div class="row-e">-->
+<!--            <div class="col-1-2">-->
+<!--                123-->
+<!--            </div>-->
+<!--            <div class="col-1-2">-->
+<!--                123-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
+
+    <div class="row-e">
+        <div class="col-1">
             <?= $special_offer ?>
         </div>
     </div>
-</div>
 
+</div>
 <?php
 $script = <<< JS
 
         $("#owl_lend_products_product-card").owlCarousel({
-          items : 4, //10 изображений на 1000px 
-          itemsDesktop : [1000,4], //5 изображений на ширину между 1000px и 901px
-          itemsDesktopSmall : [900,3], // 3 изображения между 900px и 601px
-          itemsTablet: [600,2], //2 изображения между 600 и 0;
-          itemsMobile : false,
-          navigation : true
+          items : 4 //10 изображений на 1000px 
         });
 
 JS;
