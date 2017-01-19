3646b23537b293153e2d9aab7142e110e3c789ff
Nitsenko <nitsenko94@gmail.com>
2017-01-19 03:48:20 +0300

    [task] Переделан header tpl

--- frontend/themes/om/resources/partial/cat-landing/header/default.php
+++ frontend/themes/om/resources/partial/cat-landing/header/default.php
@@ -1,97 +1,106 @@
-<div class="row-e">
+<div class="header-top-row">
     <div class="col-1">
-        <div class="header_bar" style="height: 55px;border-bottom: 1px solid rgb(204, 204, 204);">
-            <div class="top-link-cont" style="padding: 12px 9px; float: right; text-align: right;">
-                <div style="background: rgb(255, 191, 8) none repeat scroll 0% 0%; font-size: 12px; float: right; position: relative; right: 35px;"
-                     class="cart-count badge">5
-                </div>
-                <a rel="nofollow" class="top-link" href="/glavnaya/cart"><i class="fa fa-shopping-cart"
-                                                                            style="font-size: 28px; color: rgb(0, 165, 161);"></i></a>
+        <div class="header-top-row__info" style="">
+            <div class="header-top-row__info__date-period">
+                <span>Пн - Пт 9:00 - 19:00</span>
             </div>
-            <div class="header_bar_img">
-                <img src="/images/logo/OM_code.png">
+            <div class="header-top-row__info__phone-number">
+                <span>8 800-123-45-67</span>
+            </div>
+            <div class="header-top-row__info__lk">
+                <span class="header-top-row__info__lk__btn btn_arrow-down">Личный кабинет</span>
             </div>
         </div>
     </div>
 </div>
 
-<div class="special_header">
-    <h1><?= ($header_title) ?: 'Специальное предложение' ?></h1>
-</div>
+<div class="o-container col-95">
+
+    <div class="header-actions-bar">
+        <div class="col-3-10" style="background: black">
+            <div class="header-actions-bar__logo">
+                <div class="header-actions-bar__logo__img">
+                    <img src="" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="col-4-10">
+            <div class="header-actions-bar__search">
+                <form style="width: 100%;height: 100%; ">
+                    <input class="header-actions-bar__shopping-basket__input" type="text">
+                </form>
+                <div class="header-actions-bar__shopping-basket__submit">
+                    <span>Найти</span>
+                </div>
+            </div>
+        </div>
+        <div class="col-3-10">
+            <div class="header-actions-bar__shopping-basket">
+                <div class="top-link-cont" style="padding: 12px 9px; float: right; text-align: right;">
+                    <div style="background: rgb(255, 191, 8) none repeat scroll 0% 0%; font-size: 12px; float: right; position: relative; right: 35px;"
+                         class="cart-count badge">5
+                    </div>
+                    <a rel="nofollow" class="top-link" href="/glavnaya/cart"><i class="fa fa-shopping-cart"
+                                                                                style="font-size: 28px; color: rgb(0, 165, 161);"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="header-nav-bar">
+            <div class="header-nav-bar__list">
+                <div class="header-nav-bar__list_b list_disc">
+                    <a class="header-nav-bar__list_a list_a list_a-new" href="#">Новинки</a></div>
+                <div class="header-nav-bar__list_b list_disc">
+                    <a class="header-nav-bar__list_a list_a" href="#">Женщинам</a></div>
+                <div class="header-nav-bar__list_b list_disc">
+                    <a class="header-nav-bar__list_a list_a" href="#">Мужчинам</a></div>
+                <div class="header-nav-bar__list_b list_disc">
+                    <a class="header-nav-bar__list_a list_a" href="#">Детям</a></div>
+                <div class="header-nav-bar__list_b list_disc">
+                    <a class="header-nav-bar__list_a list_a" href="#">Обувь</a></div>
+                <div class="header-nav-bar__list_b list_disc">
+                    <a class="header-nav-bar__list_a list_a" href="#">Сумки</a></div>
+                <div class="header-nav-bar__list_b list_disc">
+                    <a class="header-nav-bar__list_a list_a" href="#">Аксессуары</a></div>
+                <div class="header-nav-bar__list_b list_disc">
+                    <a class="header-nav-bar__list_a list_a" href="#">Бижутерия и украшения</a></div>
+                <div class="header-nav-bar__list_b list_disc">
+                    <a class="header-nav-bar__list_a list_a" href="#">Дом и дача</a></div>
+                <div class="header-nav-bar__list_b list_disc">
+                    <a class="header-nav-bar__list_a list_a" href="#">Красота и здоровье</a></div>
+                <div class="header-nav-bar__list_b list_disc">
+                    <a class="header-nav-bar__list_a list_a" href="#">Игрушки</a></div>
+                <div class="header-nav-bar__list_b list_disc">
+                    <a class="header-nav-bar__list_a list_a" href="#">Спорт и туризм</a></div>
+                <div class="header-nav-bar__list_b list_disc">
+                    <a class="header-nav-bar__list_a list_a" href="#">Хобби и увлечения</a></div>
+                <div class="header-nav-bar__list_b list_disc">
+                    <a class="header-nav-bar__list_a list_a" href="#">Книги</a></div>
+                <div class="header-nav-bar__list_b list_disc">
+                    <a class="header-nav-bar__list_a list_a" href="#">Офис ишкола</a></div>
+                <div class="header-nav-bar__list_b list_disc">
+                    <a class="header-nav-bar__list_a list_a" href="#">Электроника</a></div>
+                <div class="header-nav-bar__list_b list_disc">
+                    <a class="header-nav-bar__list_a list_a" href="#">Автотовары</a></div>
+                <div class="header-nav-bar__list_b list_disc">
+                    <a class="header-nav-bar__list_a list_a" href="#">Спецодежда</a></div>
+                <div class="header-nav-bar__list_b list_disc">
+                    <a class="header-nav-bar__list_a list_a" href="#">Зоотовары</a></div>
+                <div class="header-nav-bar__list_b list_disc">
+                    <a class="header-nav-bar__list_a list_a" href="#">Бренды</a></div>
+                <div class="header-nav-bar__list_b list_disc">
+                    <a class="header-nav-bar__list_a list_a list_a-discount" href="#">Распродажа</a></div>
+            </div>
+    </div>
 
 <!-- banner -->
 <?= \frontend\widgets\MainBanner::widget($banner_config); ?>
 <div class="clearfix"></div>
 <!-- end banner -->
 
-<!--footer banner -->
-<div class="row-e" style="margin-top: 25px">
-    <div class="col-1">
-        <a href="https://www.instagram.com/odezhda_master/" target="_blank"><img
-                    style="display: block;max-width: 100%;height: auto;" src="/images/banners/OM_14122016_6.png"
-                    alt="Топ 5 лучших товаров каждый день"></a>
-    </div>
+<div class="col-1 special_header_c-h2"><?= ($header_title) ?: 'Специальное предложение' ?></div>
+<div class="clearfix"></div>
+
 </div>
-<!-- end banner -->
-
-
-
-<div id="cat-products">
-    <div class="row-e">
-        <div class="col-1">
-            <div class="row-e" style="padding: 15px; border-bottom: 1px solid rgb(204, 204, 204);">
-                <div class="col-1-5" style="margin-top: 10px">
-                    <div class="row-e">
-                        <div class="col-1-3">
-                            <img width="100%" src="/images/logo/low_price.png">
-                        </div>
-                        <div class="col-2-3 about_text">
-                            Пожалуй, самые низкие цены
-                        </div>
-                    </div>
-                </div>
-                <div class="col-1-5" style="margin-top: 10px">
-                    <div class="row-e">
-                        <div class="col-1-3">
-                            <img width="100%" src="/images/logo/orders.png">
-                        </div>
-                        <div class="col-2-3 about_text">
-                            Нами выполненно более 1,000,000 заказов
-                        </div>
-                    </div>
-                </div>
-                <div class="col-1-5" style="margin-top: 10px">
-                    <div class="row-e">
-                        <div class="col-1-3">
-                            <img width="100%" src="/images/logo/vipolneno.png">
-                        </div>
-                        <div class="col-2-3 about_text">
-                            Более 25,000 товаров для вашего выбора
-                        </div>
-                    </div>
-                </div>
-                <div class="col-1-5" style="margin-top: 10px">
-                    <div class="row-e">
-                        <div class="col-1-3">
-                            <img width="100%" src="/images/logo/novinki.png">
-                        </div>
-                        <div class="col-2-3 about_text">
-                            Новинки каждый день
-                        </div>
-                    </div>
-                </div>
-                <div class="col-1-5" style="margin-top: 10px">
-                    <div class="row-e">
-                        <div class="col-1-3">
-                            <img width="100%" src="/images/logo/minorder.png">
-
-                        </div>
-                        <div class="col-2-3 about_text">
-                            Минимальный заказ всего от 5000 рублей
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+<div class="clearfix"></div>
