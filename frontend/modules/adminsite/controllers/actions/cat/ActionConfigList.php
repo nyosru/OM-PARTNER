bf36a743328ef0c7706716a73d8ecb78abe82ded
Nitsenko <nitsenko94@gmail.com>
2017-01-23 09:07:49 +0300

    [task] Удаляю папку, чтобы ее не было в выходных данных

--- frontend/modules/adminsite/controllers/actions/cat/ActionConfigList.php
+++ frontend/modules/adminsite/controllers/actions/cat/ActionConfigList.php
@@ -14,6 +14,10 @@ trait ActionConfigList
         $scandir_cat_configs = array_diff(scandir(\Yii::getAlias('@runtime') . '/cat/'), ['..', '.']);
         $scandir_cat_configs = array_values($scandir_cat_configs);
 
+        // удаляем папку для предпросмотра, чтобы не мозолил глаза
+        $preview_key = array_search('special_offers', $scandir_cat_configs);
+        unset($scandir_cat_configs[$preview_key]);
+
         foreach ($scandir_cat_configs as $key => $name) {
             $scandir_cat_configs[$key] = stristr($name, '.', true);
         }
