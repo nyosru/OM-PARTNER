739baff2d8ea681c572a53b69ca3f6113a5ad8b5
Nitsenko <nitsenko94@gmail.com>
2017-01-23 09:01:56 +0300

    [task] Убрал удаление документа с превью, теперь его нет

--- frontend/modules/adminsite/controllers/actions/cat/ActionConfigList.php
+++ frontend/modules/adminsite/controllers/actions/cat/ActionConfigList.php
@@ -14,10 +14,6 @@ trait ActionConfigList
         $scandir_cat_configs = array_diff(scandir(\Yii::getAlias('@runtime') . '/cat/'), ['..', '.']);
         $scandir_cat_configs = array_values($scandir_cat_configs);
 
-        // удаляем файл для предпросмотра, чтобы не мозолил глаза
-        $preview_key = array_search('preview_config.json', $scandir_cat_configs);
-        unset($scandir_cat_configs[$preview_key]);
-
         foreach ($scandir_cat_configs as $key => $name) {
             $scandir_cat_configs[$key] = stristr($name, '.', true);
         }
