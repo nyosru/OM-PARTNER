7cd8b38d77ca0076ffe593e86ab82f0373a5c4a7
desure85@gmail.com <Oleinum85>
2017-02-04 00:00:29 +0300

    Upd

--- vendor/yiisoft/extensions.php
+++ vendor/yiisoft/extensions.php
@@ -462,4 +462,13 @@ return array (
       '@kartik/time' => $vendorDir . '/kartik-v/yii2-widget-timepicker',
     ),
   ),
+  'yiisoft/yii2-httpclient' => 
+  array (
+    'name' => 'yiisoft/yii2-httpclient',
+    'version' => '2.0.2.0',
+    'alias' => 
+    array (
+      '@yii/httpclient' => $vendorDir . '/yiisoft/yii2-httpclient',
+    ),
+  ),
 );
