a48bfc2c0a16a66fd37bb56a98be2b9346b52980
desure85@gmail.com <Oleinum85>
2017-02-03 13:24:36 +0300

    Sp update ref percent

--- frontend/modules/sp/controllers/actions/ActionSetPercent.php
+++ frontend/modules/sp/controllers/actions/ActionSetPercent.php
@@ -13,6 +13,17 @@ trait ActionSetPercent
 {
     public function actionSetPercent()
     {
+        if(($percent = (integer)Yii::$app->request->post('percent')) == TRUE && $percent >= 0 && $percent <= 50){
+            $referal = Referrals::find()->where(['user_id'=>Yii::$app->user->id])->one();
+            $referal->percent = $percent;
+            if($referal->save()){
+                return 'Значение сохранено';
+            }else{
+                return 'Не удалось задать значение';
+            }
 
+        }else{
+            return 'Значение должно быть от 0до 50';
+        }
     }
 }
